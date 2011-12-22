76db37f4ea1261843e843c8b3339ef00370addf0
fjbenitog <fjbenitog@554b9c65-5a4d-0410-bcd4-bcaad269303d>
2011-12-22 12:24:45 +0000

    New Images

--- viewerfree-gwt/trunk/src/main/java/es/viewerfree/gwt/client/viewer/ViewerPanel.java
+++ viewerfree-gwt/trunk/src/main/java/es/viewerfree/gwt/client/viewer/ViewerPanel.java
@@ -3,15 +3,18 @@ package es.viewerfree.gwt.client.viewer;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.LayoutPanel;
 import com.google.gwt.user.client.ui.SplitLayoutPanel;
-
-import es.viewerfree.gwt.client.util.ErrorMessageUtil;
 
 public class ViewerPanel extends SplitLayoutPanel {
 	
 	private FoldersListPanel foldersListPanel;
 	
-	private FolderPanel rightPanel;
+	private FolderPanel floderPanel;
+	
+	private ImagesPanel imagesPanel;
+
+	private LayoutPanel rightPanel;
 	
 	public ViewerPanel() {
 		super(7);
@@ -19,6 +22,14 @@ public class ViewerPanel extends SplitLayoutPanel {
 		add(getRightPanel());
 	}
 
+	private LayoutPanel getRightPanel(){
+		if(this.rightPanel == null){
+			this.rightPanel = new LayoutPanel();
+			this.rightPanel.add(getFolderPanel());
+		}
+		return this.rightPanel;
+	}
+	
 	private FoldersListPanel getFoldersListPanel(){
 		if(this.foldersListPanel == null){
 			this.foldersListPanel = new FoldersListPanel();
@@ -27,28 +38,43 @@ public class ViewerPanel extends SplitLayoutPanel {
 	}
 	
 	
-	private FolderPanel getRightPanel(){
-		if(this.rightPanel == null){
-			this.rightPanel = new FolderPanel();
+	private FolderPanel getFolderPanel(){
+		if(this.floderPanel == null){
+			this.floderPanel = new FolderPanel();
 		}
-		return this.rightPanel;
+		return this.floderPanel;
+	}
+	
+	private ImagesPanel getImagesPanel(){
+		if(this.imagesPanel == null){
+			this.imagesPanel = new ImagesPanel();
+		}
+		return this.imagesPanel;
 	}
 	
 	public void setAlbumsList(String[] albums){
-		AlbumClickHandler albumClickHandler = new AlbumClickHandler();
+		AlbumClickHandler albumClickHandler = new AlbumClickHandler(this);
 		for (String album : albums) {
-			getRightPanel().addFolder(album,albumClickHandler);
+			getFolderPanel().addFolder(album,albumClickHandler);
 			getFoldersListPanel().addFolder(album,albumClickHandler);
 		}
 	}
 	
 	private static class AlbumClickHandler implements ClickHandler{
 
+		private ViewerPanel viewerPanel;
+		
+		public AlbumClickHandler(ViewerPanel viewerPanel) {
+			this.viewerPanel = viewerPanel;
+		}
+
 		@Override
 		public void onClick(ClickEvent clickevent) {
 			if(clickevent.getSource() instanceof HTML){
+				viewerPanel.getRightPanel().clear();
 				HTML albumLink = (HTML) clickevent.getSource();
-				ErrorMessageUtil.getErrorDialogBox(albumLink.getText());
+				viewerPanel.getImagesPanel().addImage(albumLink.getText(), null);
+				viewerPanel.getRightPanel().add(viewerPanel.getImagesPanel());
 			}
 			
 		}
