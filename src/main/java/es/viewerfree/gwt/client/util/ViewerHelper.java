3714d9698ea8b81712e50326c0e19af364b3656f
fjbenitog <fjbenitog@554b9c65-5a4d-0410-bcd4-bcaad269303d>
2011-12-30 00:11:37 +0000

    Added image detail

---
+++ src/main/java/es/viewerfree/gwt/client/util/ViewerHelper.java
@@ -0,0 +1,20 @@
+package es.viewerfree.gwt.client.util;
+
+import com.google.gwt.core.client.GWT;
+
+import es.viewerfree.gwt.shared.Action;
+import es.viewerfree.gwt.shared.ParamKey;
+
+public class ViewerHelper {
+
+	private static final String IMAGE_SERVICE = "imageService";
+	
+	public static String createUrlImage(final String albumName, String imageName, Action action) {
+		StringBuffer urlImage = new StringBuffer();
+		urlImage.append(GWT.getModuleBaseURL()).append(IMAGE_SERVICE+"?")
+		.append(ParamKey.ALBUM_NAME).append("=").append(albumName)
+		.append("&").append(ParamKey.PICTURE_NAME).append("=").append(imageName)
+		.append("&").append(ParamKey.ACTION).append("=").append(action);
+		return urlImage.toString();
+	}
+}
--- src/main/java/es/viewerfree/gwt/client/viewer/ImagesPanel.java
+++ src/main/java/es/viewerfree/gwt/client/viewer/ImagesPanel.java
@@ -2,41 +2,35 @@ package es.viewerfree.gwt.client.viewer;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.Image;
 import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.LayoutPanel;
-import com.google.gwt.user.client.ui.PopupPanel;
 import com.google.gwt.user.client.ui.ScrollPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import com.reveregroup.gwt.imagepreloader.ImageLoadEvent;
-import com.reveregroup.gwt.imagepreloader.ImageLoadHandler;
-import com.reveregroup.gwt.imagepreloader.ImagePreloader;
 
 import es.viewerfree.gwt.client.ViewerFreeMessages;
+import es.viewerfree.gwt.client.common.FitImageLoader;
 import es.viewerfree.gwt.client.service.ViewerService;
 import es.viewerfree.gwt.client.service.ViewerServiceAsync;
 import es.viewerfree.gwt.client.util.ErrorMessageUtil;
+import es.viewerfree.gwt.client.util.ViewerHelper;
 import es.viewerfree.gwt.shared.Action;
-import es.viewerfree.gwt.shared.ParamKey;
 
 public class ImagesPanel extends LayoutPanel {
 
 	private final ViewerServiceAsync viewerService = GWT.create(ViewerService.class);
-	
+
 	private final ViewerFreeMessages messages = GWT.create(ViewerFreeMessages.class);
 
 	private FlowPanel imagesPanel;
-	
+
 	private LayoutPanel albumTitlePanel;
-	
+
 	private Label albumTitleLabel;
-	
+
 	private ScrollPanel scrollImagesPanel;
 
 	public ImagesPanel() {
@@ -45,7 +39,7 @@ public class ImagesPanel extends LayoutPanel {
 		add(getScrollImagesPanel());
 		setWidgetTopBottom(getScrollImagesPanel(), 70, Unit.PX, 0, Unit.PX);
 	}
-	
+
 	private LayoutPanel getAlbumTitlePanel(){
 		if(this.albumTitlePanel == null){
 			this.albumTitlePanel = new LayoutPanel();
@@ -56,7 +50,7 @@ public class ImagesPanel extends LayoutPanel {
 		}
 		return this.albumTitlePanel;
 	}
-	
+
 	private Label getAlbumTitleLabel(){
 		if(this.albumTitleLabel == null){
 			this.albumTitleLabel = new Label();
@@ -73,7 +67,7 @@ public class ImagesPanel extends LayoutPanel {
 		}
 		return this.scrollImagesPanel;
 	}
-	
+
 	private FlowPanel getImagesPanel(){
 		if(this.imagesPanel == null){
 			this.imagesPanel = new FlowPanel();
@@ -85,14 +79,15 @@ public class ImagesPanel extends LayoutPanel {
 		getAlbumTitleLabel().setText(albumName);
 		viewerService.getPictures(albumName, new CallGetPictures(albumName));
 	}
-	
+
 	public void clear(){
 		getImagesPanel().clear();
 	}
-	
+
 	private final class CallGetPictures implements AsyncCallback<String[]> {
 
-		private static final String IMAGE_SERVICE = "imageService";
+
+		private static final int imageSize = 150;
 		private final String albumName;
 
 		protected CallGetPictures(String albumName) {
@@ -102,11 +97,10 @@ public class ImagesPanel extends LayoutPanel {
 		@Override
 		public void onSuccess(String[] images) {
 			for (String imageName : images) {
-				final Image loaderImage = new Image("images/ajax-loader.gif");
-				final HorizontalPanel imagePanel = createImagePanel(loaderImage);
-				getImagesPanel().add(imagePanel);
-				ImagePreloader.load(createUrlImage(albumName, imageName, Action.SHOW_THUMBNAIL),
-						new ImageLoaderHandlerImpl(loaderImage, imagePanel,imageName));
+				final FitImageLoader loaderImage = new FitImageLoader("images/ajax-loader.gif",
+						ViewerHelper.createUrlImage(albumName, imageName, Action.SHOW_THUMBNAIL),imageSize,imageSize);
+				loaderImage.addClickHandler(new ShowImageHandler(albumName, imageName));
+				getImagesPanel().add(createImagePanel(loaderImage));
 			}
 		}
 
@@ -114,66 +108,16 @@ public class ImagesPanel extends LayoutPanel {
 		public void onFailure(Throwable arg0) {
 			ErrorMessageUtil.getErrorDialogBox(messages.serverError());
 		}
-		
 
-		
 		private HorizontalPanel createImagePanel(final Image loaderImage) {
 			final HorizontalPanel imagePanel = new HorizontalPanel();
+			imagePanel.setSize(imageSize+"px", imageSize+"px");
 			imagePanel.setStyleName("image");
-			imagePanel.setWidth("150px");
-			imagePanel.add(loaderImage);
 			imagePanel.setVerticalAlignment(VerticalPanel.ALIGN_MIDDLE);
+			imagePanel.setHorizontalAlignment(HorizontalPanel.ALIGN_CENTER);
+			imagePanel.add(loaderImage);
 			return imagePanel;
 		}
-		
-		private String createUrlImage(final String albumName, String imageName, Action action) {
-			StringBuffer urlImage = new StringBuffer();
-			urlImage.append(GWT.getModuleBaseURL()).append(IMAGE_SERVICE+"?")
-					.append(ParamKey.ALBUM_NAME).append("=").append(albumName)
-					.append("&").append(ParamKey.PICTURE_NAME).append("=").append(imageName)
-					.append("&").append(ParamKey.ACTION).append("=").append(action);
-			return urlImage.toString();
-		}
-		
-		private final class ImageLoaderHandlerImpl implements ImageLoadHandler {
-			private final Image loaderImage;
-			private final HorizontalPanel imagePanel;
-			private final String imageName;
-			
-			private ImageLoaderHandlerImpl(Image loaderImage,
-					HorizontalPanel imagePanel,String imageName) {
-				this.loaderImage = loaderImage;
-				this.imagePanel = imagePanel;
-				this.imageName = imageName;
-			}
-			
-			@Override
-			public void imageLoaded(ImageLoadEvent event) {
-				imagePanel.remove(loaderImage);
-				addImage(imagePanel, event.takeImage());
-			}
-			
-			private void addImage(final HorizontalPanel imagePanel, Image image) {
-				image.addClickHandler(new ShowImageHandler());
-				imagePanel.add(image);
-				imagePanel.setCellHorizontalAlignment(image, HorizontalPanel.ALIGN_CENTER);
-				imagePanel.setCellVerticalAlignment(image, VerticalPanel.ALIGN_MIDDLE);
-			}
-			
-			private final class ShowImageHandler implements ClickHandler{
-				
-				@Override
-				public void onClick(ClickEvent event) {
-					PopupPanel slidePanel = new PopupPanel();
-					slidePanel.add(new Image(createUrlImage(albumName, imageName, Action.SHOW_PICTURE)));
-					slidePanel.setGlassEnabled(true);
-					
-					slidePanel.setPopupPosition((Window.getClientWidth()-800)/2, 0);
-					slidePanel.show();
-				}
-				
-			}
-		}
-		
+
 	}
 }
