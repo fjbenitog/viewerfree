4258b706fa8c591a1e27a7bb66ba4f01478f4999
fjbenitog <fjbenitog@554b9c65-5a4d-0410-bcd4-bcaad269303d>
2014-01-02 23:01:07 +0000

    Added login RestService

--- viewerfree-gwt/trunk/src/main/java/es/viewerfree/gwt/server/service/rest/impl/RestViewerService.java
+++ viewerfree-gwt/trunk/src/main/java/es/viewerfree/gwt/server/service/rest/impl/RestViewerService.java
@@ -4,20 +4,44 @@ import java.util.Date;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import es.viewerfree.gwt.server.service.IUserService;
 import es.viewerfree.gwt.server.service.rest.IRestViewerService;
+import es.viewerfree.gwt.server.util.CriptoUtilTest;
+import es.viewerfree.gwt.server.util.CryptoUtil;
+import es.viewerfree.gwt.shared.dto.UserDto;
 
 @Path("/")
 @Produces("text/plain")
 public class RestViewerService implements IRestViewerService {
 
+	@Autowired
+	private IUserService userService;
+	
 	@Override
 	@GET
     @Path("/time")
 	public String getTime() {
 		// TODO Auto-generated method stub
 		return new Date().toString();
+	}
+
+	@Override
+	@GET
+    @Path("/login/{authorization}")
+	public String login(@PathParam("authorization") String authorization)  throws Exception{
+		String values[] = authorization.split("\\@");
+		UserDto credentials = userService.getCredentials(values[0], values[1]);
+		if(credentials!=null){
+			return CryptoUtil.encrypt(credentials.getName()+":"+System.currentTimeMillis(), credentials.getName());
+		}else{
+			return "";
+		}
+		
 	}
 
 }
